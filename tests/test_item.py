8d51a577ef10fcb0beb63ed841d5ac9396422cd6
Andrey Ryazanov <ark2635@gmail.com>
2023-06-19 13:16:50 +0300

    homework_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+item1 = Item("Гаджет", 5000, 2)
+
+
+def test_init():
+    assert item1.price == 5000
+    assert item1.name == "Гаджет"
+    assert item1.quantity == 2
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 10000
+
+
+def test_apply_discount():
+    assert item1.apply_discount() == None
