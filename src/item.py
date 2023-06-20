53e896343d18df593666876e881a1fcd56ed82b3
Andrey Ryazanov <ark2635@gmail.com>
2023-06-20 16:35:51 +0300

    homework_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
@@ -31,3 +34,32 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """Получает название товара
+        :return: Название товара"""
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        """проверяет, что длина наименования товара не больше 10 симвовов.
+        В противном случае, обрезает строку (оставить первые 10 символов)."""
+        if len(value) <= 10:
+            self.__name = value
+        else:
+            self.__name = value[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv"""
+        cls.all = []
+        with open('E:\electronics-shop-project\src\items.csv') as file:
+            reader = csv.DictReader(file, delimiter=',')
+            for line in reader:
+                cls(str(line['name']), float(line['price']), int(line['quantity']))
+
+    @staticmethod
+    def string_to_number(value):
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(value))
--- tests/test_item.py
+++ tests/test_item.py
@@ -14,5 +14,26 @@ def test_calculate_total_price():
     assert item1.calculate_total_price() == 10000
 
 
+def test_item1_quantity():
+    assert item1.quantity == 2
+
+
 def test_apply_discount():
     assert item1.apply_discount() == None
+
+
+def test_item_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_item1_from_csv_name():
+    Item.instantiate_from_csv()
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_item_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
