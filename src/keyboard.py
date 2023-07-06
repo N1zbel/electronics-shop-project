da52f44e34ea7172e9632b40eb6e9ec55410ae0a
Andrey Ryazanov <ark2635@gmail.com>
2023-07-06 10:30:13 +0300

    homework_5

---
+++ src/keyboard.py
@@ -0,0 +1,27 @@
+from src.item import Item
+
+
+class MixinLang:
+
+    def __init__(self):
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'RU':
+            self.__language = 'EN'
+            return self
+        else:
+            self.__language = 'RU'
+            return self
+
+
+
+class KeyBoard(Item, MixinLang):
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        MixinLang.__init__(self)
---
+++ tests/test_keyboard.py
@@ -0,0 +1,11 @@
+import pytest
+from src.keyboard import KeyBoard
+
+def test_init():
+    kb = KeyBoard('razer', 9000, 15)
+    assert str(kb) == "razer"
+    assert str(kb.language) == "EN"
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+    kb.change_lang().change_lang()
+    assert str(kb.language) == "RU"
